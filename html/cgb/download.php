8f8727496c5082ca554b251c050675cc301f5107
Matze167435 <80071873+Sudel-Matze@users.noreply.github.com>
2022-12-12 21:52:00 +0100

    Preliminary fix for authenticated downloads

--- html/cgb/download.php
+++ html/cgb/download.php
@@ -4,6 +4,12 @@ define('CORE_PATH', dirname(dirname(__DIR__)) . '/core');
 require_once(CORE_PATH.'/core.php');
 require_once(CORE_PATH.'/auth.php');
 
-	doAuth();
-    serveFileOrExecScript($_GET["name"], "download");
+	$sessionId = doAuth(1);
+	if (isset($sessionId)) {
+		if (is_int($sessionId) && $sessionId == 0) {
+			serveFileOrExecScript($_GET["name"], "download");
+		} else {
+			serveFileOrExecScript($_GET["name"], "download", $sessionId);
+		}
+	}
 ?>
