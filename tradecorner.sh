c7ef19bb755de59295c43c3181c8d7e4b9ff8999
Matando <matthew@strelsky.net>
2023-01-18 13:36:48 -0500

    Add NGINX and Systemd examples

---
+++ tradecorner.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+dotnet /var/www/gameboy.datacenter.ne.jp/app/crystal-trade-corner/CGBPokemonCronJob/bin/Debug/netcoreapp6/CGBPokemonCronJob.dll bxtj /var/www/gameboy.datacenter.ne.jp/config.json
+dotnet /var/www/gameboy.datacenter.ne.jp/app/crystal-trade-corner/CGBPokemonCronJob/bin/Debug/netcoreapp6/CGBPokemonCronJob.dll bxte /var/www/gameboy.datacenter.ne.jp/config.json
