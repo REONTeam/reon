d1dd305fd4705faf2e71da4dabc6f2b94ed2dc32
HilbertIdeals5 <prayag.suthar@icloud.com>
2025-12-15 21:51:52 -0500

    Revised account page input validation for Trade Corner setting
    It's rough, but it's the most straightforward fix I can think of at the moment, and I think there's a promising basis here for adding more user settings in the future.

--- web/htdocs/user/summary.php
+++ web/htdocs/user/summary.php
@@ -6,6 +6,21 @@
 	
 	if (SessionUtil::getInstance()->isSessionActive()) {
 		$db_util = DBUtil::getInstance();
+  
+        $errors = array(); //~To contain multiple problems at once
+        //~Update user settings if needed, before preparing to render the page
+        if (array_key_exists("tradeRegions",$_POST)) {
+            if (
+                in_array($_POST["tradeRegions"],array("e,f,d,s,i,p,u,j","efdsipu,j","efdsipuj"))
+            ) {
+                $db = DBUtil::getInstance()->getDB();
+                $stmt = $db->prepare("update sys_users set trade_region_allowlist = ? where id = ?");
+                $stmt->bind_param("si", $_POST["tradeRegions"], $_SESSION["user_id"]);
+                $stmt->execute();
+            } else { //~If region setting is invalid, make no changes and issue an error
+                $errors[] = "regionValue";
+            }
+        }
 		
 		$db = $db_util->getDB();
 		$stmt = $db->prepare("select email, dion_ppp_id, dion_email_local, log_in_password, money_spent, trade_region_allowlist from sys_users where id = ?");
@@ -26,7 +41,8 @@
 			"log_in_password" => $result["log_in_password"],
 			"money_spent" => $result["money_spent"],
             "trade_region_allowlist" => $result["trade_region_allowlist"],
-			"inbox_size" => $inbox_size
+			"inbox_size" => $inbox_size,
+            "errors" => $errors
 		]);
 	} else {
 		header("Location: /index.php");
